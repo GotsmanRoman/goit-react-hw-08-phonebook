a9fe245f77f0087a5b3b0d03525ee7a9f12a410b
GotsmanRoman <96074289+GotsmanRoman@users.noreply.github.com>
2023-06-11 01:51:23 +0300

    Update Header.jsx

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -8,8 +8,6 @@ import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
-import IconButton from '@mui/material/IconButton';
-import MenuIcon from '@mui/icons-material/Menu';
 import styles from './Header.module.css';
 
 export function Header() {
